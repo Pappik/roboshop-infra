4f7327af6a0b208242cbc713649e4a15adae8f3a
pappik <pappik@gmail.com>
2023-12-28 20:47:49 -0500

    jenkins

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "null_resource" "test" {
-  provisioner "local-exec" {
-    command = "echo ${env}"
+module "vpc" {
+  source = "github.com/Pappik/tf-module-vpc"
+  for_each = var.vpc
+  cidr_block = each.value.cidr_block
   }
-}
