14fa6bd4c542e3afa907232511977d28ac1ea40d
pappik <pappik@gmail.com>
2024-01-02 19:55:53 -0500

    vpc terraform

--- main.tf
+++ main.tf
@@ -6,8 +6,4 @@ module "vpc" {
 
   for_each = var.vpc
   cidr_block = each.value.cidr_block
-  public_subnets_cidr = each.value.private_subnets_cidr
-  private_subnets_cidr = each.value.private_subnets_cidr
-  availability_zones = each.value.availability_zones
-
 }
