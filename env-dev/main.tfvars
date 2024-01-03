d10b53d051b221468919c7ea752a334e5d9306cf
pappik <pappik@gmail.com>
2024-01-02 19:14:22 -0500

    vpc terraform

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -6,7 +6,7 @@ vpc = {
     cidr_block = "10.0.0.0/16"
     public_subnets_cidr = ["10.0.0.0/17", "10.0.1.0/17"]
     private_subnets_cidr = ["10.0.3.0/17", "10.0.4.0/17"]
-
+    availability_zones   = ["us-east-1a", "us-east-1b"]
 
   }
 }
--- main.tf
+++ main.tf
@@ -8,5 +8,6 @@ module "vpc" {
   cidr_block = each.value.cidr_block
   public_subnets_cidr = each.value.private_subnets_cidr
   private_subnets_cidr = each.value.private_subnets_cidr
+  availability_zones = each.value.availability_zones
 
 }
