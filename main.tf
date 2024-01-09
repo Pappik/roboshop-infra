93fbde3f2baedb18f41514c3e688b72a5b960cd6
pappik <pappik@gmail.com>
2024-01-09 11:46:42 -0500

    vpc terraform

--- main.tf
+++ main.tf
@@ -77,19 +77,19 @@ module "alb" {
  subnets_name = each.value.subnets_name
   internal = each.value.internal
 }
-#
-#module "apps" {
-#  source = "github.com/Pappik/tf-module-apps"
-#  env    = var.env
-#
-#  for_each = var.apps
-#  subnet_ids = lookup(lookup(lookup(lookup(module.vpc, "main" , null),each.value.subnets_type, null),each.value.subnets_name, null),"subnet_ids", null)
-#  vpc_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
-#  allow_cidr = lookup(lookup(lookup(lookup(module.vpc, "main" , null), each.value.allow_cidr_subnets_type, null), each.value.allow_cidr_subnets_name, null),"cidr_block", null)
-#  component   = each.value.component
-#  app_port    = each.value.app_port
-#}
-#
-#output "vpc" {
-#  value = module.vpc
-#}
+
+module "apps" {
+  source = "github.com/Pappik/tf-module-apps"
+  env    = var.env
+
+  for_each = var.apps
+  subnet_ids = lookup(lookup(lookup(lookup(module.vpc, "main" , null),each.value.subnets_type, null),each.value.subnets_name, null),"subnet_ids", null)
+  vpc_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+  allow_cidr = lookup(lookup(lookup(lookup(module.vpc, "main" , null), each.value.allow_cidr_subnets_type, null), each.value.allow_cidr_subnets_name, null),"cidr_block", null)
+  component   = each.value.component
+  app_port    = each.value.app_port
+}
+
+output "vpc" {
+  value = module.vpc
+}
