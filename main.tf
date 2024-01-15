b973a35dcb4c41983ae77ace5d21a0832cdf8d06
pappik <pappik@gmail.com>
2024-01-15 12:48:54 -0500

    vpc terraform

--- main.tf
+++ main.tf
@@ -75,8 +75,8 @@ module "alb" {
   for_each = var.alb
   subnet_ids = lookup(lookup(lookup(lookup(module.vpc, "main" , null),each.value.subnets_type, null),each.value.subnets_name, null),"subnet_ids", null)
   vpc_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
-  allow_cidr = lookup(lookup(lookup(lookup(module.vpc, "main" , null),"private_subnet_ids", null), "app", null),"cidr_block", null)
- subnets_name = each.value.subnets_name
+  allow_cidr = each.value.internal ? lookup(lookup(lookup(lookup(module.vpc, "main" , null),"private_subnet_ids", null), "app", null),"cidr_block", null) : ["0.0.0.0/0"]
+  subnets_name = each.value.subnets_name
   internal = each.value.internal
 }
 
@@ -92,6 +92,7 @@ module "apps" {
   allow_cidr = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), each.value.allow_cidr_subnets_type, null), each.value.allow_cidr_subnets_name, null),"cidr_block", null)
   alb      = lookup(lookup(module.alb, each.value.alb, null), "dns_name", null)
   listener  = lookup(lookup(module.alb, each.value.alb, null), "listener", null)
+  alb_arn  = lookup(lookup(module.alb, each.value.alb, null), "alb_arn", null)
   component   = each.value.component
   app_port    = each.value.app_port
   max_size = each.value.max_size
